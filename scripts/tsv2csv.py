3fcda4d854cbc8504a2731c79744cd2fd1d33d48
congosto <mariluz.congosto@gmail.com>
2023-01-05 10:26:57 +0100

    Check if the source variable exists

--- scripts/tsv2csv.py
+++ scripts/tsv2csv.py
@@ -15,18 +15,19 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see
 # <http://www.gnu.org/licenses/>.
-
+from __future__ import print_function
 import os
 import sys
 import codecs
 import csv
-import unicodecsv as csv
+#import unicodecsv as csv
+from imp import reload
 import argparse
 
 def main():
-  reload(sys)
-  sys.setdefaultencoding('utf-8')
-  sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+  #reload(sys)
+  #sys.setdefaultencoding('utf-8')
+  #sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
   #defino argumentos de script
   parser = argparse.ArgumentParser(description='This script generates a file in json format for visualization in d3.js')
   parser.add_argument('file_in', type=str, help='file with data')
@@ -42,7 +43,7 @@ def main():
   try:  
     f_in = codecs.open(file_in, 'rU')
   except:
-    print 'Can not open file',file_in
+    print ('Can not open file',file_in)
     exit (1)
   prefix,ext=os.path.splitext(file_in)
   if size == 0:
@@ -59,7 +60,7 @@ def main():
       row_head=row
       head=False
     if num_line % 10000 == 0:
-      print num_line
+      print (num_line)
     num_line +=1
     num_line_block += 1
     writer.writerow(row)
@@ -67,17 +68,17 @@ def main():
       f_out.close()
       num_pack += 1
       num_line_block =0
-      f_out=f=codecs.open(prefix+'_'+str(num_pack)+'.csv', 'w',encoding='utf-8',errors='ignore')
+      f_out=codecs.open(prefix+'_'+str(num_pack)+'.csv', 'w',encoding='utf-8',errors='ignore')
       writer = csv.writer(f_out,delimiter=',')
       writer.writerow(row_head)
   f_in.close()
   f_out.close()
-  print "converted file %s to %s.csv" % (file_in,prefix)
+  print ("converted file %s to %s.csv" % (file_in,prefix))
   exit(0)
 
 if __name__ == '__main__':
   try:
     main()
   except KeyboardInterrupt:
-    print '\nGoodbye! '
+    print ('\nGoodbye! ')
  
